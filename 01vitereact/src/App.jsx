64a0df8f2621c729ab8f413472d032528140fd1d
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-25 13:06:51 +0500

    understand the react flow and and structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,20 @@
+import Chai from "./Chai";
+
 function App() {
   return (
-    <h1
-      style={{ textAlign: "center", backgroundColor: "orange", color: "blue" }}
-    >
-      Umair Abbasi
-    </h1>
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+          backgroundColor: "orange",
+          color: "blue",
+        }}
+      >
+        Umair Abbasi
+      </h1>
+      
+      <Chai />
+    </>
   );
 }
 
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,5 @@
+function Chai() {
+  return <h3 style={{textAlign:"center",color:"red"}}>learning React  is a wounderful experienced</h3>;
+}
+
+export default Chai;
