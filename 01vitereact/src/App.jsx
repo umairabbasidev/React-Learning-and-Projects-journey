6d10758508d8d1c24c780f6bdbb2423c2ea8e45c
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-25 12:26:57 +0500

    understand creation of react project using vite and cra

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <h1
+      style={{ textAlign: "center", backgroundColor: "orange", color: "blue" }}
+    >
+      Umair Abbasi
+    </h1>
+  );
+}
+
+export default App;
