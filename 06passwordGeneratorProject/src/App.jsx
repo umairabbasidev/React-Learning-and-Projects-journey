c1ac9e9a4bf8a7b9836bb090cced83a01413cc34
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-04-01 16:06:40 +0500

    passwordGeneratorProject completed

--- 06passwordGeneratorProject/src/App.jsx
+++ 06passwordGeneratorProject/src/App.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
 document.body.style.backgroundColor = "#212121";
 document.body.style.color = "#fff";
 function App() {
   let [length, setLength] = useState(8);
   let [numberAllowed, setNumberAllowed] = useState(false);
   let [characterAllowed, setCharacterAllowed] = useState(false);
+  let [password, setPassword] = useState("");
 
-  
+  // useRef
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%&*_+";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0, 3);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
+
   return (
     <>
       <h1 className="text-3xl font-bold text-center mt-5">
@@ -17,11 +44,16 @@ function App() {
         <div className="flex   ">
           <input
             type="text"
+            value={password}
             placeholder="Password"
             className="outline-none border-0 w-full p-2 bg-white text-orange-800  bg-transparent rounded-l-md"
             readOnly
+            ref={passwordRef}
           />
-          <button className="bg-blue-800 text-white p-2 rounded-r-md">
+          <button
+            className="bg-blue-700 text-white p-2 rounded-r-md hover:bg-blue-600"
+            onClick={copyPasswordToClipboard}
+          >
             Copy
           </button>
         </div>
@@ -29,10 +61,9 @@ function App() {
           <div className="flex items-center gap-2 lg:flex-nowrap flex-wrap">
             <input
               type="range"
-              name=""
-              id=""
               max={100}
               min={6}
+              value={length}
               onChange={(e) => {
                 setLength(e.target.value);
               }}
@@ -42,7 +73,6 @@ function App() {
             <div className=" flex gap-1 ">
               <input
                 type="checkbox"
-                name=""
                 id="inputElement"
                 defaultChecked={numberAllowed}
                 onChange={() => {
